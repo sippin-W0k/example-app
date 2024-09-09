8af4f89af32a7aba884b5a8729b0b654ab63fc1e
sippin-W0k <outkast66@mail.ru>
2024-09-09 13:04:13 +0500

    create first route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/first', function () {
+    $a = 1;
+    $b = 2;
+    $c = $a + $b;
+    dd($c);
+    
+
+    return view('first',compact('c'));
+});
+
